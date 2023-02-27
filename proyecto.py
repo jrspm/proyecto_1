66a9cb24d4218a41dd95999a84cc4aef09657fc5
jrspm <j.ricardo.merida@gmail.com>
2023-02-27 13:40:45 -0600

    actualizacion_5

--- proyecto.py
+++ proyecto.py
@@ -4,6 +4,63 @@ import pandas as pd
 
 import random
 
+
+############################################# inicio ########################################
+
+#cargar datos con el nombre del archivo y el numero de columnas, tambien puede reducir en un porcentaje 
+
+class Datos_Proyecto:
+
+    def __init__(self, nombre_archivo,lista_columnas): # todos los atributos del objeto
+
+        self.array_total=np.load(nombre_archivo) #cargar el archivo usando el nombre, debe estar en la carpeta y deber ser un array
+
+        self.lista_columnas_1=lista_columnas #lista del nombre de las columnas de array
+
+        rows, columns = self.array_total.shape #obtener cantidad de filas, columnas del array
+
+        self.cantidad_filas=rows #cantidad de filas del array
+
+        self.array_tolist=self.array_total.tolist() # convertir en lista el array para poder usarlo en pandas
+
+        self.dataframe_total=pd.DataFrame(self.array_tolist, columns=self.lista_columnas_1) # crear el dataframe y nombrar las columnas
+
+
+    def funcion_1(self,porcentaje_reducir): #metodo para reducir el dataframe, porcentaje a reducir
+
+        lista_reducir=[]
+
+        if porcentaje_reducir<1 and porcentaje_reducir>0: # condicionante para reducir
+
+            cantidad_filas_porcentaje=int((self.cantidad_filas)*porcentaje_reducir) #cantidad de filas del porcentaje a reducir
+
+
+            while len(lista_reducir)<cantidad_filas_porcentaje: # terminar el proceso hasta que se tengan todos los valores de la lista
+
+                valor_random=random.randint(0,self.cantidad_filas-1) # seleccionar un valor random, entre 0 y la cantidad de datos
+
+                if lista_reducir.count(valor_random)==0: # comparar que no exista el valor random
+                
+                    lista_reducir.append(valor_random) #agregar a la lista el valor random
+            
+            
+            lista_reducir.sort() # ordenar la lista a reducir
+
+            set_range_cantida_filas=set(list(range(self.cantidad_filas-1))) # conjunto de todas las filas dataframe
+
+            lista_reducida_trabajar=list(set_range_cantida_filas.difference(set(lista_reducir))) #diferencia de conjuntos, lista de los valores a trabajar
+
+            data_Reducido_trabajar=self.dataframe_total.drop(lista_reducir, axis=0) #dataframe de las filas que se trabajaran
+
+            data_Reducido_comparar=self.dataframe_total.drop(lista_reducida_trabajar, axis=0) #dataframe de las filas que se compararan
+
+            return data_Reducido_trabajar, data_Reducido_comparar
+
+        else:
+
+            print("Valor incorrecto")
+
+
 lista_1=["PRECIO",
     "CALIDAD_MATERIAL",
     "AREA_PISO",
@@ -11,47 +68,19 @@ lista_1=["PRECIO",
     "AÑO_CONSTRUCCION",
     "FRENTE"]
 
-#print(lista_1)
-
-class Datos_Proyecto:
-
-    def __init__(self, nombre_archivo,lista_columnas):
-
-        self.array_total=np.load(nombre_archivo)
-        self.lista_columnas_1=lista_columnas
-
-        rows, columns = self.array_total.shape
-
-        self.cantidad_filas=rows
-
-        self.cantidad_filas_porcentaje=0
-
-
-
-    def funcion_1(self,porcentaje_reducir):
-
-        if porcentaje_reducir<1 and porcentaje_reducir>0:
-
-            self.cantidad_filas_porcentaje=(self.cantidad_filas)*porcentaje_reducir
-
-        else:
-
-            print("Valor incorrecto")
-
-        return self.array_total
-
-
 datos_1=Datos_Proyecto("proyecto_training_data.npy",lista_1)
 
 
-array_1=datos_1.funcion_1(0.2)
-
-print(array_1)
+data_Reducido_from_datos_1=datos_1.funcion_1(0.2)
 
 
+print(datos_1.dataframe_total)
 
+print(data_Reducido_from_datos_1[0])
 
+print(data_Reducido_from_datos_1[1])
 
+input()
 
 #matrix_1Reducida=dataframe_elemento.drop(lista_surtida, axis=1)
 
